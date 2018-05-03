bec3e79d3c16ac0593f747b11aa9d4a7804ec99b
yuliyuan <312326701@qq.com>
2018-05-03 15:58:46 +0800

    1

--- js/personal-mymessage.js
+++ js/personal-mymessage.js
@@ -1,14 +1,18 @@
 function fun() {
     $('.readed').removeClass('active');
     $('.read').removeClass('active');
+    $('.one').css('display', 'none');
+    $('.two').css('display', 'none');
 }
 $(function() {
     $('.readed').click(function() {
         fun();
         $(this).addClass('active');
+        $('.one').css('display', 'block');
     })
     $('.read').click(function() {
         fun();
         $(this).addClass('active');
+        $('.two').css('display', 'block');
     })
 })
